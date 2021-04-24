69df64a974472f2853d1c39c1a24599fec77b234
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2021-04-24 13:08:01 +0900

    8. UI와 로직 분리 ViewModel - kotlin
    1. java와 동일한 방법으로 ViewModel 생성
    2. suspend 한정자를 통해 코루틴에서 처리할 함수를 명시 가능

--- room_exam_kotlin/app/src/main/java/com/hyeonwoo/room_exam_kotlin/MainActivity.kt
+++ room_exam_kotlin/app/src/main/java/com/hyeonwoo/room_exam_kotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hyeonwoo.room_exam_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,17 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-name")
-            .allowMainThreadQueries()
-            .build()
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
-        db.todoDao().getAll().observe(this, Observer {
+        viewModel.getAll().observe(this, Observer {
             result_text.text = it.toString()
         })
 
         add_button.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
-                db.todoDao().insert(Todo(todo_edit.text.toString())) 
+                viewModel.insert(Todo(todo_edit.text.toString()))
             }
         }
     }
---
+++ room_exam_kotlin/app/src/main/java/com/hyeonwoo/room_exam_kotlin/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.hyeonwoo.room_exam_kotlin
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+    private val db = Room.databaseBuilder(application, AppDatabase::class.java, "tood-db").build()
+
+    fun getAll(): LiveData<List<Todo>> {
+        return db.todoDao().getAll();
+    }
+    /*
+     suspend 한정자는 해당 함수가 무조건 코루틴 안에서 처리되어야 한다는 뜻으로,
+     코루틴 스코프를 벗어나면 컴파일러에서 에러로 처리한다.
+     */
+    suspend fun insert(todo : Todo) {
+        db.todoDao().insert(todo)
+    }
+
+
+}
